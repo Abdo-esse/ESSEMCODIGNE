4019b877a36778bd2ad9089dcaa48f7494b7d0b1
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-11-21 12:07:35 +0100

    j'ai termai   la function de affiche modal

---
+++ asset/script/main.js
@@ -0,0 +1,36 @@
+//button add Task Btn
+const addTaskBtn=document.querySelector('.addTaskBtn')
+
+//Div ouverly
+const ouverly=document.querySelector('.ouverly')
+
+//div modal
+const modal=document.querySelector('#modal')
+
+// svg de ferme modal
+const fermeMoldalBtn=document.querySelector('.fermeModalBtn')
+
+
+
+
+//Declaration des function
+
+//Declaration du fonction d'affichage modal
+function addTask() {
+   ouverly.style.display= "block";
+   modal.style.display= "block";
+    
+}
+
+//Declaration du fonction de ferme modal
+function fermeModal() {
+    ouverly.style.display= "none";
+    modal.style.display= "none";
+}
+
+
+
+//appel des functions
+
+// L'appel du function  d'affichage modal
+addTaskBtn.addEventListener('click',addTask)
